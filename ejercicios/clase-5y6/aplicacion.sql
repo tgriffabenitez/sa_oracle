24fd2d5d00a3841c7ecb1135fc1140534e52bf8d
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-30 14:22:25 -0300

    update

--- ejercicios/clase-5y6/aplicacion.sql
+++ ejercicios/clase-5y6/aplicacion.sql
@@ -1,46 +1,32 @@
 DECLARE
-  L_EMPNO NUMBER;
-  L_JOB VARCHAR2(200);
-  L_MGR NUMBER;
-  L_SAL NUMBER;
-  L_COMM NUMBER;
-  L_DEPTNO NUMBER;
-  L_SALARYCHANGE NUMBER;
+    L_EMPNO NUMBER;
+    L_JOB VARCHAR2(200);
+    L_MGR NUMBER;
+    L_SAL NUMBER;
+    L_COMM NUMBER;
+    L_DEPTNO NUMBER;
+    L_SALARYCHANGE NUMBER;
+    
 BEGIN
-  L_EMPNO := 147;
-  L_JOB := 'SA_MAN';
-  L_MGR := 100;
-  L_SAL := 12000;
-  L_COMM := 0.3;
-  L_DEPTNO := 80;
+    L_EMPNO := 147;
+    L_JOB := 'SA_MAN';
+    L_MGR := 100;
+    L_SAL := 25000;
+    L_COMM := 0.3;
+    L_DEPTNO := 80;
 
-  PROMOTE(
-    L_EMPNO => L_EMPNO,
-    L_JOB => L_JOB,
-    L_MGR => L_MGR,
-    L_SAL => L_SAL,
-    L_COMM => L_COMM,
-    L_DEPTNO => L_DEPTNO,
-    L_SALARYCHANGE => L_SALARYCHANGE
-  );
-  /* Legacy output: 
-DBMS_OUTPUT.PUT_LINE('L_SALARYCHANGE = ' || L_SALARYCHANGE);
-*/ 
-  :L_SALARYCHANGE := L_SALARYCHANGE;
+    PROMOTE(
+        L_EMPNO => L_EMPNO,
+        L_JOB => L_JOB,
+        L_MGR => L_MGR,
+        L_SAL => L_SAL,
+        L_COMM => L_COMM,
+        L_DEPTNO => L_DEPTNO,
+        L_SALARYCHANGE => L_SALARYCHANGE
+    );
+
+    DBMS_OUTPUT.PUT_LINE('L_SALARYCHANGE = ' || L_SALARYCHANGE);
+
+    :L_SALARYCHANGE := L_SALARYCHANGE;
 --rollback; 
 END;
-
-SELECT
-    *
-FROM
-    employees emp
-    JOIN departments dep ON emp.department_id = dep.department_id
-    JOIN jobs j ON emp.job_id = j.job_id
-WHERE
-    emp.employee_id = 147;
-    
-    
-SELECT 
-    *
-FROM 
-    job_history
