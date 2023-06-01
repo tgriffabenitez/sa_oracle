68eafff1364f4f03432cd5e5722e9af210113113
tgriffabenitez <tgriffabenitez@gmail.com>
2023-06-01 00:08:52 -0300

    Se modificaron los nombres de las variables y metodos ya que ahora ejecuto el package desde el usuario 'ABM_HR'

--- ejercicios/trabajo-final/aplicacion.sql
+++ ejercicios/trabajo-final/aplicacion.sql
@@ -1,20 +1,20 @@
 -- AGREGAR EMPLEADO ------------------------------------------------------------
 DECLARE
-    v_first_name     employees.first_name%TYPE := 'TOMAS';
-    v_last_name      employees.last_name%TYPE := 'GRIFFA';
-    v_email          employees.email%TYPE := 'tgriffa';
-    v_phone_number   employees.phone_number%TYPE := '111.111.1111';
-    v_hire_date      employees.hire_date%TYPE := TO_DATE ( '15-May-23', 'DD-MON-YY' );
-    v_job_id         employees.job_id%TYPE := 'IT_PROG';
-    v_salary         employees.salary%TYPE := 15000;
-    v_commission_pct employees.commission_pct%TYPE := 0.2;
-    v_manager_id     employees.manager_id%TYPE := 112;
-    v_department_id  employees.department_id%TYPE := 60;
+    v_first_name     hr.employees.first_name%TYPE := 'TOMAS';
+    v_last_name      hr.employees.last_name%TYPE := 'GRIFFA';
+    v_email          hr.employees.email%TYPE := 'tgriffa';
+    v_phone_number   hr.employees.phone_number%TYPE := '111.111.1111';
+    v_hire_date      hr.employees.hire_date%TYPE := TO_DATE ( '15-May-23', 'DD-MON-YY' );
+    v_job_id         hr.employees.job_id%TYPE := 'IT_PROG';
+    v_salary         hr.employees.salary%TYPE := 15000;
+    v_commission_pct hr.employees.commission_pct%TYPE := 0.2;
+    v_manager_id     hr.employees.manager_id%TYPE := 112;
+    v_department_id  hr.employees.department_id%TYPE := 60;
     v_error_msg      VARCHAR2(200);
     v_result         BOOLEAN;
 BEGIN
     -- Llamo a la funcion agregar_empleado()
-    v_result := pa_gestion_hr.agregar_empleado(v_first_name, v_last_name, v_email, v_phone_number, v_hire_date,
+    v_result := hr.pa_gestion_hr.agregar_empleado(v_first_name, v_last_name, v_email, v_phone_number, v_hire_date,
                                               v_job_id, v_salary, v_commission_pct, v_manager_id, v_department_id,
                                               v_error_msg);
 
@@ -28,12 +28,12 @@ END;
 
 -- ELIMINAR EMPLEADO -----------------------------------------------------------
 DECLARE
-    v_employee_id employees.employee_id%TYPE := 209;
+    v_employee_id hr.employees.employee_id%TYPE := 210;
     v_error_msg   VARCHAR2(200);
     v_result      BOOLEAN;
 BEGIN
     -- Llamo a la función eliminar_empleado()
-    v_result := pa_gestion_hr.eliminar_empleado(v_employee_id, v_error_msg);
+    v_result := hr.pa_gestion_hr.eliminar_empleado(v_employee_id, v_error_msg);
     IF v_result THEN
         dbms_output.put_line('El empleado ha sido eliminado exitosamente.');
     ELSE
@@ -44,22 +44,22 @@ END;
 
 -- MODIFICAR EMPLEADO ----------------------------------------------------------
 DECLARE
-    v_employee_id    employees.employee_id%TYPE := 999;
-    v_first_name     employees.first_name%TYPE := 'modificado';
-    v_last_name      employees.last_name%TYPE := 'modificado';
-    v_email          employees.email%TYPE := 'modificado';
-    v_phone_number   employees.phone_number%TYPE := '111.111.1111';
-    v_hire_date      employees.hire_date%TYPE := TO_DATE ( '15-May-23', 'DD-MON-YY' );
-    v_job_id         employees.job_id%TYPE := 'IT_PROG';
-    v_salary         employees.salary%TYPE := 7777;
-    v_commission_pct employees.commission_pct%TYPE := NULL;
-    v_manager_id     employees.manager_id%TYPE := 112;
-    v_department_id  employees.department_id%TYPE := 60;
+    v_employee_id    hr.employees.employee_id%TYPE := 102;
+    v_first_name     hr.employees.first_name%TYPE := 'PEPE';
+    v_last_name      hr.employees.last_name%TYPE := 'ARGENTO';
+    v_email          hr.employees.email%TYPE := 'PARGENTO';
+    v_phone_number   hr.employees.phone_number%TYPE := '111.111.1111';
+    v_hire_date      hr.employees.hire_date%TYPE := TO_DATE ( '15-May-23', 'DD-MON-YY' );
+    v_job_id         hr.employees.job_id%TYPE := 'IT_PROG';
+    v_salary         hr.employees.salary%TYPE := 500;
+    v_commission_pct hr.employees.commission_pct%TYPE := NULL;
+    v_manager_id     hr.employees.manager_id%TYPE := 104;
+    v_department_id  hr.employees.department_id%TYPE := 60;
     v_error_msg      VARCHAR2(200);
     v_result         BOOLEAN;
 BEGIN
     -- Llamo a la funcion modificar_empleado()
-    v_result := pa_gestion_hr.modificar_empleado(v_employee_id, v_first_name, v_last_name, v_email, v_phone_number,
+    v_result := hr.pa_gestion_hr.modificar_empleado(v_employee_id, v_first_name, v_last_name, v_email, v_phone_number,
                                                 v_hire_date, v_job_id, v_salary, v_commission_pct, v_manager_id,
                                                 v_department_id, v_error_msg);
 
@@ -73,14 +73,14 @@ END;
 
 -- AGREGAR JOB ----------------------------------------------------------------
 DECLARE
-    v_job_id     jobs.job_id%TYPE := 'PRUE';
-    v_job_title  jobs.job_title%TYPE := 'HOLAHOLA';
-    v_min_salary jobs.min_salary%TYPE := 1;
-    v_max_salary jobs.max_salary%TYPE := 10000;
+    v_job_id     hr.jobs.job_id%TYPE := 'PRUE';
+    v_job_title  hr.jobs.job_title%TYPE := 'HOLAHOLA';
+    v_min_salary hr.jobs.min_salary%TYPE := 1;
+    v_max_salary hr.jobs.max_salary%TYPE := 10000;
     v_error_msg  VARCHAR2(200);
     v_result     BOOLEAN;
 BEGIN
-    v_result := pa_gestion_hr.agregar_puesto(v_job_id, v_job_title, v_min_salary, v_max_salary, v_error_msg);
+    v_result := hr.pa_gestion_hr.agregar_puesto(v_job_id, v_job_title, v_min_salary, v_max_salary, v_error_msg);
     IF v_result THEN
         dbms_output.put_line('El puesto ha sido agregado exitosamente.');
     ELSE
@@ -91,11 +91,11 @@ END;
 
 -- ELIMINAR JOB ---------------------------------------------------------------
 DECLARE
-    v_job_id    jobs.job_id%TYPE := 'IT_PROG';
+    v_job_id    hr.jobs.job_id%TYPE := 'IT_PROG';
     v_error_msg VARCHAR2(200);
     v_result    BOOLEAN;
 BEGIN
-    v_result := pa_gestion_hr.eliminar_puesto(v_job_id, v_error_msg);
+    v_result := hr.pa_gestion_hr.eliminar_puesto(v_job_id, v_error_msg);
     IF v_result THEN
         dbms_output.put_line('El puesto ha sido eliminado exitosamente.');
     ELSE
@@ -106,14 +106,14 @@ END;
 
 -- MODIFICAR JOB
 DECLARE
-    v_job_id     jobs.job_id%TYPE := 'PRUE';
-    v_job_title  jobs.job_title%TYPE := 'HOLAHOLA';
-    v_min_salary jobs.min_salary%TYPE := 1;
-    v_max_salary jobs.max_salary%TYPE := 10000;
+    v_job_id     hr.jobs.job_id%TYPE := 'PRUE';
+    v_job_title  hr.jobs.job_title%TYPE := 'HOLAHOLA';
+    v_min_salary hr.jobs.min_salary%TYPE := 1;
+    v_max_salary hr.jobs.max_salary%TYPE := 10000;
     v_error_msg  VARCHAR2(200);
     v_result     BOOLEAN;
 BEGIN
-    v_result := pa_gestion_hr.agregar_puesto(v_job_id, v_job_title, v_min_salary, v_max_salary, v_error_msg);
+    v_result := hr.pa_gestion_hr.agregar_puesto(v_job_id, v_job_title, v_min_salary, v_max_salary, v_error_msg);
     IF v_result THEN
         dbms_output.put_line('El puesto ha sido agregado exitosamente.');
     ELSE
